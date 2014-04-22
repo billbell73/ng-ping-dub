215f5402374f4d4e4e1f1dbee5de2c813c4d7384
Will Bell <willbell@gmail.com>
2014-04-22 19:52:24 +0100

    Add doubles functionality to scorer partial

--- app/scripts/directives.js
+++ app/scripts/directives.js
@@ -1,17 +1,17 @@
 var pingpongDirectives = angular.module('pingpongDirectives', []);
 
-pingpongDirectives.directive('serveball1', function() {
+pingpongDirectives.directive('serveball', function() {
+
   return {
     restrict: "E",
-    template: '<img id="p1marker" class="p1serveball"' + 
-    						' ng-show="score.isP1Serving" src="images/serveball.svg">'
+    replace: true,
+    scope: {
+      pairnumber: '@'
+    },
+    template: '<img id="p{{pairnumber}}marker" class="p{{pairnumber}}serveball"' + 
+               'src="images/serveball.svg">'
   }
 })
 
-pingpongDirectives.directive('serveball2', function() {
-  return {
-    restrict: "E",
-    template: '<img id="p2marker" class="p2serveball"' + 
-               'ng-show="!score.isP1Serving" src="images/serveball.svg">'
-  }
-})
+
+
